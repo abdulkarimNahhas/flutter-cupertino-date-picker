d0a7732b53ee222b8bf16034593ee76a7d3cf8bd
Abdulkarim <abdulkarim.alnnhas1@gmail.com>
2025-06-27 00:11:04 +0400

    integrate with OSOS

---
+++ lib/src/date_time_formats_collection.dart
@@ -0,0 +1,18 @@
+
+class DateTimeFormatsCollection {
+  final String datePickerFormat;
+  final String timePickerFormat;
+  final String monthYearPickerFormat;
+  final String datetimePickerFormat;
+  final String monthPickerFormat;
+  final String yearPickerFormat;
+
+  DateTimeFormatsCollection({
+    required this.datePickerFormat,
+    required this.timePickerFormat,
+    required this.monthYearPickerFormat,
+    required this.datetimePickerFormat,
+    required this.monthPickerFormat,
+    required this.yearPickerFormat,
+  });
+}
